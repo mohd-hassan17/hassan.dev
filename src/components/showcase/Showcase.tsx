cec37aa40c2692864a96a8f94ea6c5c79db4426f
mohd-hassan17 <ansarimohdhassan3@gmail.com>
2025-07-08 15:09:49 +0530

    add change

--- src/components/showcase/Showcase.tsx
+++ src/components/showcase/Showcase.tsx
@@ -9,29 +9,29 @@ import styles from "./showcase.module.scss";
 export const Showcase = () => {
 
   const projects = [
-     {
+    {
       title: "CoDrive",
       img: "/assets/codrive.jpg",
       code: "https://github.com/mohd-hassan17/CoDrive",
       link: "https://storeit-tawny.vercel.app/",
       tech: ["Next.js", "appwrite", "Reactjs", "JavaScript", "Figma"],
-      desc: "This is a description.",
-      modal: <>This is the modal content</>,
+      desc: "CoDrive is a collaborative file-sharing and management platform inspired by Google Drive.",
+      modal: <>CoDrive is a collaborative file-sharing and management platform inspired by Google Drive. It enables users to upload, organize, share, and collaborate on files in real-time, with a focus on simplicity, security, and teamwork.</>,
     },
-     {
+    {
       title: "Brainwave",
       img: "/gallery/brainwave.webp",
       code: "https://github.com/mohd-hassan17/",
       link: "https://brainwave-fv.netlify.app",
       tech: ["React", "Tailwind", "Vite"],
-      desc: "This is a description.",
+      desc: "Brainwave is a modern UI/UX website built using React.js and Tailwind  CSS, designed with contemporary principles in mind.",
       modal: <>Brainwave is a modern UI/UX website built using React.js and Tailwind
         CSS, designed with contemporary principles in mind. It features smooth
         parallax effects and bento box layouts, offering a stylish UI that
         adapts well from desktop to mobile devices. With polished animations.
       </>,
     },
-  
+
     {
       title: "iPhone 15",
       img: "/gallery/iphone.webp",
@@ -47,18 +47,18 @@ export const Showcase = () => {
       code: "https://github.com/mohd-hassan17/Klimate",
       link: "https://klimate-wine.vercel.app/",
       tech: ["Next.js", "Reactjs", "TypeScript", "TanStack"],
-      desc: "This is a description.",
-      modal: <>This is the modal content</>,
+      desc: "Klimate is ideal for users who want a minimalistic, fast, and reliable weather app that instantly adapts to their location without manual input.",
+      modal: <>Klimate is a modern, responsive weather application that delivers accurate, real-time weather updates based on the user’s current location. Built using TanStack Query (React Query) for efficient data fetching and caching, Klimate focuses on speed, usability, and clean UI.</>,
     },
-    
+
     {
       title: "Codemate",
       img: "/assets/codemate1.jpg",
       code: "https://github.com/mohd-hassan17/code-reviewer",
       link: "https://codemate-dun.vercel.app/",
       tech: ["Next.js", "TypeScript", "openAI", "Tailwind", "appwrite"],
-      desc: "This is a description.",
-      modal: <>This is the modal content</>,
+      desc: "AI Code Reviewer is an intelligent tool that helps developers automatically review their code for bugs, bad practices.",
+      modal: <>AI Code Reviewer is an intelligent tool that helps developers automatically review their code for bugs, bad practices, and performance issues using AI-powered analysis. It offers feedback, suggestions, and even refactoring tips—acting like a virtual senior developer reviewing your pull requests.</>,
     },
 
   ];
@@ -80,7 +80,7 @@ export const Showcase = () => {
         })}
       </div>
 
-   
+
     </section>
   );
 };
