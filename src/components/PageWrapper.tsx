4c35403549df60350a413e7cf9816d3397a0ffc3
mohd-hassan17 <mohdhassana04@gmail.com>
2025-07-08 21:07:21 +0530

    updatee

--- src/components/PageWrapper.tsx
+++ src/components/PageWrapper.tsx
@@ -14,7 +14,7 @@ export default function PageWrapper({ children }: { children: React.ReactNode })
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
         exit={{ opacity: 0, y: -20 }}
-        transition={{ duration: 0.7, ease: 'easeInOut' }}
+        transition={{ duration: 0.5, ease: 'easeInOut' }}
       >
         {children}
       </motion.div>
