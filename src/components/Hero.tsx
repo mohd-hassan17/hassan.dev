d48b84ea4c2d87c0ec85d34342b1a300bd6f2eb4
mohd-hassan17 <mohdhassana04@gmail.com>
2025-09-14 01:11:41 +0530

    new changess

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,7 +10,7 @@ const Hero = () => {
         <Link href="#" className="profile-grid-box bg-gredient">
           <div className="profile-box-text  ">
             <h1 className="font-bold text-3xl">Hi, Mohd Hassan.</h1>
-            <p>{`I'm a GenAI Full Stack Developer focused on creating innovative AI driven applications that are scalable, efficient, and user focused.`}</p>
+            <p>{`I design and build full-stack web applications powered by Generative AI, creating intuitive, high-performance, and user-focused experiences that bring ideas to life.`}</p>
             <Image src="/assets/icon.svg" width={50} height={50} alt="icon" />
           </div>
         </Link>
