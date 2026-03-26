0ed573cb0d980c868f1d0ba08bde217e4a292e69
mohd-hassan17 <mohdhassana04@gmail.com>
2026-03-26 18:52:30 +0530

    ui changes in para

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,19 +6,8 @@ import '../lib/fontawesome';
 import Header from "@/components/Header";
 import ContactSection from "@/components/Contact";
 
-import { Space_Grotesk, Plus_Jakarta_Sans } from 'next/font/google'
 
-const spaceGrotesk = Space_Grotesk({
-  subsets: ['latin'],
-  variable: '--font-heading',
-  weight: ['400', '500', '700'],
-})
 
-const plusJakarta = Plus_Jakarta_Sans({
-  subsets: ['latin'],
-  variable: '--font-body',
-  weight: ['400', '500'],
-})
 const poppins = Poppins({
   subsets: ['latin'],
   weight: ['400', '600', '700'], // Add weights you need
